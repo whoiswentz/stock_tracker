20c80e822464c1a81725cf464384330e6a4d48cd
Vinicios Wentz <wentz@alchemists.stream>
2023-07-21 14:25:32 -0300

    feat: min function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn min(series: &[f64]) -> Option<f64> {
+    if series.is_empty() {
+        None
+    } else {
+        Some(series.iter().fold(f64::MAX, |acc, n| acc.max(*n)))
+    }
+}
+
