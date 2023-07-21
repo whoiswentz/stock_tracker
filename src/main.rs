aa363098e519fc38b779a4c3d127e0fe9095a734
Vinicios Wentz <wentz@alchemists.stream>
2023-07-21 14:26:57 -0300

    feat: max function and min function changes

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ fn min(series: &[f64]) -> Option<f64> {
     if series.is_empty() {
         None
     } else {
-        Some(series.iter().fold(f64::MAX, |acc, n| acc.max(*n)))
+        Some(series.iter().fold(f64::MAX, |acc, n| acc.min(*n)))
     }
 }
 
+fn max(series: &[f64]) -> Option<f64> {
+    if series.is_empty() {
+        None
+    } else {
+        Some(series.iter().fold(f64::MIN, |acc, n| acc.max(*n)))
+    }
+}
