148d08fe319e30c588b70e3ced7955ea2485f4ab
Vinicios Wentz <wentz@alchemists.stream>
2023-07-21 14:51:55 -0300

    feat: price diff function

--- src/main.rs
+++ src/main.rs
@@ -17,3 +17,15 @@ fn max(series: &[f64]) -> Option<f64> {
         Some(series.iter().fold(f64::MIN, |acc, n| acc.max(*n)))
     }
 }
+
+fn price_diff(series: &[f64]) -> Option<(f64, f64)> {
+    if !series.is_empty() {
+        let (first, last) = (series.first().unwrap(), series.last().unwrap());
+        let abs_diff = last - first;
+        let first = if *first == 0.0 { 1.0 } else { *first };
+        let rel_diff = abs_diff / first;
+        Some((abs_diff, rel_diff))
+    } else {
+        None
+    }
+}
