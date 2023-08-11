efdda38a327770f1ce9998c680f70bef2b12ed9f
Vinicios Wentz <vinicios@wentz.io>
2023-08-11 12:03:43 -0300

    feat(calculation_actor): split the calculation function
    Remove the calculation from the request_symbol_data

--- src/finance/request.rs
+++ src/finance/request.rs
@@ -14,24 +14,26 @@ pub async fn request_symbol_data(
         .await
         .unwrap();
 
+    Some(prices)
+}
+
+pub async fn calculate(symbol: String, from: DateTime<Utc>, prices: Vec<f64>) -> String {
     let last_price = prices.last().unwrap();
     let (_, rel_diff) = price_diff(&prices).unwrap();
     let period_min = min(&prices).unwrap();
     let period_max = max(&prices).unwrap();
     let windows = n_window(30, &prices).unwrap();
 
-    println!(
-        "{} - {}, {}, {}, {}, {}, {}",
-        from.to_rfc3339(),
+    [
         symbol,
-        last_price,
-        rel_diff * 100.00,
-        period_min,
-        period_max,
-        windows.last().unwrap_or(&0.0)
-    );
-
-    Some(prices)
+        from.to_rfc3339(),
+        last_price.to_string(),
+        (rel_diff * 100.00).to_string(),
+        period_min.to_string(),
+        period_max.to_string(),
+        windows.last().unwrap_or(&0.0).to_string(),
+    ]
+    .join(",")
 }
 
 pub async fn request_closing_data(
